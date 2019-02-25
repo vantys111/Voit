1f2c34eb73f61624b7c822f2655995bc748524c4
Maxim Voytic <petagryskin@gmail.com>
2019-02-25 17:15:00 +0300

    three commit

--- 1day/1day/Program.cs
+++ 1day/1day/Program.cs
@@ -6,17 +6,47 @@ using System.Threading.Tasks;
 
 namespace _1day
 {
+    class Firm
+    {
+        private string name;
+        public string Name { get; set; }
+        private string addres;
+        public string Addres { get; set; }
+        private int year;
+        public int Year { get; set; }
+
+
+        public void print()
+        {
+            Console.WriteLine("Название" + Name + "Адресс" + Addres + "Год основания" + Year);
+        }
+
+        public void vrite()
+        {
+            Console.WriteLine("Введите название");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите адрес");
+            addres = Console.ReadLine();
+            Console.WriteLine("Введите год основания");
+            year = int.Parse(Console.ReadLine());
+        }
+        public Firm()
+        {
+            Name = "ооо Воробей";
+            Addres = "ул. Октябрьская 57";
+            Year = 1990;
+        }
+    }
+
     class Program
     {
-        class firm
-        {
-            public string name;
-            public string addres;
-            public int year;
+       
 
-        }
         static void Main(string[] args)
         {
+            Firm name = new Firm();
+            name.print();
+            Console.ReadKey();
         }
     }
 }
